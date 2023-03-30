5f458e70127346f57e1d65f929b8257844b635a7
Обучающийся <user@iMac-149.local>
2023-03-30 19:02:57 +0300

    30/3/2023

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const sections = [
+    document.querySelector(".home"),
+    document.querySelector(".categories"),
+    document.querySelector(".top-sellers"),
+    document.querySelector(".reviews"),
+    document.querySelector(".footer"),
+  ];
   let sectionHeights = [0];
   let home = document.querySelector(".home");
+  
   let buttons = home.querySelectorAll("button");
 
   let buildSlide = function (curSlide) {
+    home.mainColor = homeSlides[curSlide].mainColor;
+    home.subColor = homeSlides[curSlide].subColor;
     changeHeaderColor();
     home.querySelector("h1").textContent = homeSlides[curSlide].slideText;
     home.querySelector("h1").style.color = homeSlides[curSlide].subColor;
@@ -34,7 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 1000);
   };
 
-  let homeSlides = [{
+  let homeSlides = [
+    {
       mainColor: "#734222",
       subColor: "#4DBF85",
       slideText: "Подберите инструмент для любимого хобби",
@@ -66,21 +77,16 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(() => {
     curSlide = (curSlide + 1) % 3;
     buildSlide(curSlide);
-  }, 20000);
+  }, 3000);
 
   // Секция категорий
-
+  document.querySelector(".categories").mainColor = "#f5f5f5";
+  document.querySelector(".categories").subColor = "#000000";
   {
     class CategoryCard {
       constructor(
-        width, {
-          image,
-          rgbColor,
-          categoryName,
-          faIcon,
-          desc,
-          href
-        } = cardInf
+        width,
+        { image, rgbColor, categoryName, faIcon, desc, href } = cardInf
       ) {
         this.image = image;
         this.rgbColor = rgbColor;
@@ -142,26 +148,19 @@ document.addEventListener("DOMContentLoaded", () => {
         res.forEach((elem) => {
           const cards = document.querySelector(".categories .cards");
           cards.appendChild(
-            new CategoryCard(window.innerWidth, elem).compilled
+            new CategoryCard(window.screen.width, elem).compilled
           );
         });
       });
   }
 
   // Секция популярных товаров
-
+  document.querySelector(".top-sellers").mainColor = "#8fbc8f";
+  document.querySelector(".top-sellers").subColor = "#ffffff";
   class ItemCard {
     constructor(
-      width, {
-        id,
-        type,
-        color,
-        typeName,
-        name,
-        images,
-        desc,
-        price
-      } = item
+      width,
+      { id, type, color, typeName, name, images, desc, price } = item
     ) {
       this.color = color;
       this.width = width;
@@ -260,7 +259,9 @@ document.addEventListener("DOMContentLoaded", () => {
             }</h4>
             <div class="bot">
               <div class="avaib">В наличии</div>
-              <div class="price">${this.price} <span class="colored">₽</span></div>
+              <div class="price">${
+                this.price
+              } <span class="colored">₽</span></div>
             </div>
           </div>`;
       this.colorize(card);
@@ -276,132 +277,25 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then((res) => {
       const wrap = document.querySelector(".top-sellers .wrap");
-      const items = [].concat(res.acoustic.slice(0, 3), res.electric.slice(0, 3), res.ench.slice(7, 9)).sort(() => Math.random() - 0.5);
+      const items = []
+        .concat(
+          res.acoustic.slice(0, 3),
+          res.electric.slice(0, 3),
+          res.ench.slice(7, 9)
+        )
+        .sort(() => Math.random() - 0.5);
       items.forEach((item) => {
-        wrap.appendChild(new ItemCard(window.innerWidth, item).compilled);
+        wrap.appendChild(new ItemCard(window.screen.width, item).compilled);
       });
     });
 
   // Секция отзывов
-  let reviewsList = [{
-      "name": "Иван Иванов",
-      "reviewText": "Купил гитару в интернет-магазине Barett, очень доволен качеством инструмента и быстрой доставкой.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Алексей Петров",
-      "reviewText": "Никогда не покупал гитары в интернете, но Barett смог меня убедить. Купил гитару и не пожалел.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Мария Сидорова",
-      "reviewText": "Заказывала гитару в Barett для своего брата. Он остался очень доволен подарком!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Денис Иванов",
-      "reviewText": "Очень доволен своей новой гитарой от Barett. Звук просто потрясающий!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Наталья Ковалева",
-      "reviewText": "Быстро и качественно доставили гитару. Рекомендую Barett!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Сергей Семенов",
-      "reviewText": "Отличный выбор гитар в интернет-магазине Barett. Я нашел то, что искал!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Ольга Игнатова",
-      "reviewText": "Покупала гитару в Barett для своей дочери. Она счастлива!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Александра Григорьева",
-      "reviewText": "Отличный магазин гитар. Купила здесь свою первую гитару и очень довольна!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Дмитрий Королев",
-      "reviewText": "Получил свою новую гитару от Barett. Просто в восторге!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Евгений Макаров",
-      "reviewText": "Заказал гитару в Barett, доставили очень быстро. Качество на высоте!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Виктория Лебедева",
-      "reviewText": "Купила гитару в Barett для своего мужа. Он в восторге от звука!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Андрей Николаев",
-      "reviewText": "Отличный магазин гитар. Заказал гитару и получил ее на следующих день после оплаты. Качество инструмента просто потрясающее!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Елена Степанова",
-      "reviewText": "Купила гитару в Barett для своей дочери, которая начала заниматься музыкой. Гитара оказалась очень качественной!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Анна Попова",
-      "reviewText": "Спасибо Barett за быструю доставку гитары. Я уже успела ее опробовать и она звучит просто великолепно!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Михаил Кузнецов",
-      "reviewText": "Отличный магазин гитар. Заказывал у них уже несколько раз и всегда остался доволен!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Юлия Петухова",
-      "reviewText": "Купила гитару в Barett для своего сына, который играет в группе. Он остался очень доволен!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Павел Крылов",
-      "reviewText": "Качество гитары от Barett на высшем уровне. Я очень доволен своей покупкой!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    },
-    {
-      "name": "Ирина Максимова",
-      "reviewText": "Быстро и качественно доставили мою новую гитару. Спасибо Barett!.",
-      "type": "Положительный",
-      "picture": "img/home/reviews/person.jpg"
-    }
-  ];
+  document.querySelector(".reviews").mainColor = "#ffffff";
+  document.querySelector(".reviews").subColor = "#000000";
   let reviews = document.querySelector(".reviews");
 
-
   class ReviewCard {
-    constructor({
-      name,
-      reviewText,
-      type,
-      picture
-    } = item) {
+    constructor({ name, reviewText, type, picture } = item) {
       this.name = name;
       this.reviewText = reviewText;
       this.type = type;
@@ -411,9 +305,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const card = document.createElement("div");
       card.classList.add("card");
       card.innerHTML = `
-      <div class="type ${this.type == "Положительный" ? "green" : "red"}-text">${
-        this.type
-      }</div>
+      <div class="type ${
+        this.type == "Положительный" ? "green" : "red"
+      }-text">${this.type}</div>
       <div class="name">${this.name}</div>
       <p class="review">${this.reviewText}</p>
       <img class="picture" alt="profile picture" src="${this.picture}">`;
@@ -453,7 +347,6 @@ document.addEventListener("DOMContentLoaded", () => {
         arrowRight.addEventListener("click", incPage);
       }
     }
-
   }
 
   fetch("data/reviews.json")
@@ -468,15 +361,30 @@ document.addEventListener("DOMContentLoaded", () => {
         );
       }
 
-      function changePage(reviewsList, prevPage, curPage, wrap, indicators, arrowLeft, arrowRight, pageQuant) {
+      function changePage(
+        reviewsList,
+        prevPage,
+        curPage,
+        wrap,
+        indicators,
+        arrowLeft,
+        arrowRight,
+        pageQuant
+      ) {
         currentPage = curPage;
-        const page = new ReviewsPage(reviewsList, prevPage, curPage, arrowLeft, arrowRight);
+        const page = new ReviewsPage(
+          reviewsList,
+          prevPage,
+          curPage,
+          arrowLeft,
+          arrowRight
+        );
         page.renderPage(wrap);
         page.changeIndicators(indicators, arrowLeft, arrowRight, pageQuant);
       }
       let pageQuant = 3;
       let itemsPerPage = 6;
-      if (window.innerWidth < 600) {
+      if (window.screen.width < 600) {
         pageQuant = 6;
         itemsPerPage = 3;
       }
@@ -492,80 +400,71 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       indicators.querySelectorAll(".indicator").forEach((indicator, i) => {
         indicator.addEventListener("click", () => {
-          changePage(getReviewsList(i + 1, items), currentPage, i + 1, wrap, indicators.querySelectorAll(".indicator"), arrowLeft, arrowRight, pageQuant);
+          changePage(
+            getReviewsList(i + 1, items),
+            currentPage,
+            i + 1,
+            wrap,
+            indicators.querySelectorAll(".indicator"),
+            arrowLeft,
+            arrowRight,
+            pageQuant
+          );
         });
       });
       incPage = function () {
-        changePage(getReviewsList(currentPage + 1, items), currentPage, currentPage + 1, wrap, indicators.querySelectorAll(".indicator"), arrowLeft, arrowRight, pageQuant);
+        changePage(
+          getReviewsList(currentPage + 1, items),
+          currentPage,
+          currentPage + 1,
+          wrap,
+          indicators.querySelectorAll(".indicator"),
+          arrowLeft,
+          arrowRight,
+          pageQuant
+        );
       };
       decPage = function () {
-        changePage(getReviewsList(currentPage - 1, items), currentPage, currentPage - 1, wrap, indicators.querySelectorAll(".indicator"), arrowLeft, arrowRight, pageQuant);
+        changePage(
+          getReviewsList(currentPage - 1, items),
+          currentPage,
+          currentPage - 1,
+          wrap,
+          indicators.querySelectorAll(".indicator"),
+          arrowLeft,
+          arrowRight,
+          pageQuant
+        );
       };
       arrowLeft.addEventListener("click", decPage);
       arrowRight.addEventListener("click", incPage);
-      changePage(getReviewsList(1, items), 1, 1, wrap, indicators.querySelectorAll(".indicator"), arrowLeft, arrowRight, pageQuant);
-
+      changePage(
+        getReviewsList(1, items),
+        1,
+        1,
+        wrap,
+        indicators.querySelectorAll(".indicator"),
+        arrowLeft,
+        arrowRight,
+        pageQuant
+      );
     });
-
-
   window.addEventListener("scroll", changeHeaderColor);
 
-  setInterval(() => {
-    sectionHeights.push(
-      parseFloat(
-        window.getComputedStyle(document.querySelector(".home")).height
-      )
-    );
-    sectionHeights.push(
-      parseFloat(
-        window.getComputedStyle(document.querySelector(".categories")).height
-      ) + sectionHeights[1]
-    );
-    sectionHeights.push(
-      parseFloat(
-        window.getComputedStyle(document.querySelector(".top-sellers")).height
-      ) + sectionHeights[2]
-    );
-    sectionHeights.push(
-      parseFloat(
-        window.getComputedStyle(document.querySelector(".reviews")).height
-      ) + sectionHeights[3]
-    );
-  }, 2000);
+  document.querySelector(".footer").mainColor = "#49423D";
+  document.querySelector(".footer").subColor = "#ffefd5";
 
   function changeHeaderColor(e) {
-    if (
-      window.scrollY < sectionHeights[1] &&
-      window.scrollY >= sectionHeights[0]
-    ) {
-      headerColor.mainColor = homeSlides[curSlide].mainColor;
-      headerColor.subColor = homeSlides[curSlide].subColor;
-    }
-    if (
-      window.scrollY >= sectionHeights[1] &&
-      window.scrollY < sectionHeights[2]
-    ) {
-      headerColor.mainColor = "#f5f5f5";
-      headerColor.subColor = "#000000";
-    }
-    if (
-      window.scrollY >= sectionHeights[2] &&
-      window.scrollY < sectionHeights[3]
-    ) {
-      headerColor.mainColor = "#8fbc8f";
-      headerColor.subColor = "#ffffff";
-    }
-    if (
-      window.scrollY >= sectionHeights[3] &&
-      window.scrollY < sectionHeights[4]
-    ) {
-      headerColor.mainColor = "#ffffff";
-      headerColor.subColor = "#000000";
-    }
-    const header = document.querySelector("header");
-    header.style.backgroundColor = headerColor.mainColor;
-    header.querySelector(".logo").style.color = headerColor.subColor;
-    header.querySelector(".bars").style.color = headerColor.subColor;
-    header.querySelector(".cur-page").style.color = headerColor.subColor;
+    sections.forEach((elem) => {
+      if (window.scrollY >= elem.offsetTop - 80 && window.scrollY < elem.offsetTop + elem.offsetHeight - 80) {
+        console.log(elem.mainColor, elem.subColor, elem)
+        const header = document.querySelector("header");
+        header.style.backgroundColor = elem.mainColor;
+        header.querySelector(".logo").style.color = elem.subColor;
+        header.querySelector(".bars").style.color = elem.subColor;
+        header.querySelector(".cur-page").style.color = elem.subColor;
+        return;
+      }
+    });
   }
 });
